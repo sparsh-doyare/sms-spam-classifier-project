caa83289f51a020dcc0770255299e3fad592afec
sparsh-doyare <sparshronak28@gmail.com>
2026-02-08 16:25:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,50 +4,42 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
-nltk.download('punkt_tab')
 
-ps=PorterStemmer()
+# --- NLTK setup (Render-safe) ---
+REQUIRED = ["punkt", "punkt_tab", "stopwords"]
+for r in REQUIRED:
+    try:
+        nltk.data.find(
+            f"tokenizers/{r}" if "punkt" in r else f"corpora/{r}"
+        )
+    except LookupError:
+        nltk.download(r)
+
+# --- Globals ---
+ps = PorterStemmer()
+STOPWORDS = set(stopwords.words('english'))
+
 def transform_text(text):
     text = text.lower()
-    text = nltk.word_tokenize(text)
+    tokens = nltk.word_tokenize(text)
 
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
+    tokens = [i for i in tokens if i.isalnum()]
+    tokens = [i for i in tokens if i not in STOPWORDS and i not in string.punctuation]
+    tokens = [ps.stem(i) for i in tokens]
 
-    text = y[:]
-    y.clear()
+    return " ".join(tokens)
 
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
+# --- Load model ---
+tfidf = pickle.load(open("./vectorizer.pkl", "rb"))
+model = pickle.load(open("./model.pkl", "rb"))
 
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
-    return " ".join(y)
-
-tfidf=pickle.load(open("./vectorizer.pkl",'rb'))
-model=pickle.load(open("./model.pkl",'rb'))
-
+# --- UI ---
 st.title("EMAIL/SMS SPAM CLASSIFIER")
 
-input_sms=st.text_input("ENTER THE MESSAGE ")
-if st.button('Predict'):
-    #preprocessing
-    transformed_sms=transform_text(input_sms)
-    #vectorize
-    vector_input=tfidf.transform([transformed_sms])
+input_sms = st.text_input("ENTER THE MESSAGE")
+if st.button("Predict"):
+    transformed_sms = transform_text(input_sms)
+    vector_input = tfidf.transform([transformed_sms])
+    result = model.predict(vector_input)[0]
 
-
-    result=model.predict(vector_input)[0]
-
-    if result==1:
-        st.header("SPAM")
-    else:
-        st.header("NOT SPAM")
+    st.header("SPAM" if result == 1 else "NOT SPAM")
